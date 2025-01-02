950c71227d65010d4213043afba5a44ab36d939b
chargeuk <d_a_stapleton@hotmail.com>
2025-01-02 19:29:59 +0000

    VTS_Conditioning_Set_Batch_Mask supports lists

--- py/VTS_Conditioning_Set_Batch_Mask.py
+++ py/VTS_Conditioning_Set_Batch_Mask.py
@@ -15,17 +15,39 @@ class VTS_Conditioning_Set_Batch_Mask:
                               "strength": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 10.0, "step": 0.01}),
                               "set_cond_area": (["default", "mask bounds"],),
                              }}
-    RETURN_TYPES = ("CONDITIONING",)
+    RETURN_TYPES = (
+        "CONDITIONING",
+    )
+
+    # OUTPUT_IS_LIST = (
+    #     True
+    # )
+
     FUNCTION = "apply_batch_masks"
 
     CATEGORY = "VTS"
 
-    def apply_batch_masks(self, conditioning, masks, set_cond_area, strength):
-        set_area_to_bounds = False
-        if set_cond_area != "default":
-            set_area_to_bounds = True
+    @staticmethod
+    def printConditioningInfo(data, name: str):
+        if isinstance(data, list):
+            print(f"!!!VTS_Conditioning_Set_Batch_Mask item '{name}' contains a list of length [{len(data)}].")
+            # iterate through each item and call this method
+            for i, d in enumerate(data):
+                VTS_Conditioning_Set_Batch_Mask.printConditioningInfo(d, f"{name}[{i}]")
+        if isinstance(data, dict):
+            # print out the keys
+            print(f"!!!VTS_Conditioning_Set_Batch_Mask item '{name}' contains a dict with keys {data.keys()}.")
+            # iterate through each item and call this method
+            for k, v in data.items():
+                VTS_Conditioning_Set_Batch_Mask.printConditioningInfo(v, f"{name}.{k}")
+        elif hasattr(data, 'shape'):
+            print(f"!!!VTS_Conditioning_Set_Batch_Mask item '{name}' contains a conditioning shape[{len(data.shape)}]={data.shape}.")
+        else:
+            print(f"!!!VTS_Conditioning_Set_Batch_Mask item '{name}' contains an unexpected type: {type(data)}.")
+
+    @staticmethod
+    def get_conditionings_from_shape_masks(masks, conditioning, set_area_to_bounds, strength):
         conditionings = []
-        
         # Check if masks is a batch (4D tensor)
         if len(masks.shape) == 4:
             for mask in masks:
@@ -43,8 +65,41 @@ class VTS_Conditioning_Set_Batch_Mask:
                                                         "set_area_to_bounds": set_area_to_bounds,
                                                         "mask_strength": strength})
             conditionings.append(c)
+        return conditionings
+
+    @staticmethod
+    def is_list_of_multiple_conditionings(conditioning):
+        return isinstance(conditioning, list) and len(conditioning) > 0 and isinstance(conditioning[0], list) and len(conditioning[0]) > 0 and isinstance(conditioning[0][0], list) and len(conditioning[0][0]) > 0
+
+    @staticmethod
+    def is_list_of_masks(masks):
+        return isinstance(masks, list) and len(masks) > 0 and isinstance(masks[0], torch.Tensor)
+
+    def apply_batch_masks(self, conditioning, masks, set_cond_area, strength):
+        set_area_to_bounds = False
+        if set_cond_area != "default":
+            set_area_to_bounds = True
         
-        print(f"VTS conditionings: {conditionings}")
+
+        VTS_Conditioning_Set_Batch_Mask.printConditioningInfo(conditioning, "conditioning")
+
+        is_list_of_multiple_conditionings = VTS_Conditioning_Set_Batch_Mask.is_list_of_multiple_conditionings(conditioning)
+        is_list_of_masks = VTS_Conditioning_Set_Batch_Mask.is_list_of_masks(masks)
+
+        if is_list_of_multiple_conditionings != is_list_of_masks:
+            raise Exception(f"Conditioning and masks must both be lists of multiple items or not, but got conditioning: {is_list_of_multiple_conditionings} and masks: {is_list_of_masks}.")
+
+        if is_list_of_multiple_conditionings and is_list_of_masks:
+            all_conditionings = []
+            conditioning_length = len(conditioning)
+            masks_length = len(masks)
+            if conditioning_length != masks_length:
+                raise Exception(f"Conditioning length {conditioning_length} does not match masks length {masks_length}.")
+            for conditioning_item, masks_item in zip(conditioning, masks):
+                all_conditionings.append(VTS_Conditioning_Set_Batch_Mask.get_conditionings_from_shape_masks(masks_item, conditioning_item, set_area_to_bounds, strength))
+            return all_conditionings
+        
+        conditionings = VTS_Conditioning_Set_Batch_Mask.get_conditionings_from_shape_masks(masks, conditioning, set_area_to_bounds, strength)
         return conditionings
 
 # A dictionary that contains all nodes you want to export with their names
